259c9865fbc2a32ad7144901a796d60db6b130ba
NouhaylaMouakkal <nouhaylamouakkalwa19@gmail.com>
2024-10-13 20:14:25 +0100

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,7 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker'; // Import FormsModule
 
 @NgModule({
   declarations: [
@@ -20,7 +21,13 @@ import { FormsModule } from '@angular/forms'; // Import FormsModule
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule // Add FormsModule to the imports array
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }) // Add FormsModule to the imports array
   ],
   providers: [
     provideClientHydration()
